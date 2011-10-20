154274352e40e7c9523a461a084d574e2da24f61
Sebastian Wallat <sw@sw-laptop>
2011-10-20 14:59:17 +0200

    Changes in Init daemon script

--- config.py
+++ config.py
@@ -1,1 +1,36 @@
-dissomniagLive/config.py
+# -*- coding: utf-8 -*-
+"""
+Created on 28.09.2011
+
+@author: Sebastian Wallat
+"""
+
+import os
+
+try:
+    isDebug = (True if (os.environ['DEBUG'] in ("True", "1")) else False)
+except KeyError:
+    isDebug = False
+    
+execDir = (os.getcwd() if isDebug else "/usr/share/omnet/")
+
+logDir = (os.path.join(os.getcwd(), "log") if isDebug else "/var/log/")
+
+confDir = (os.getcwd() if isDebug else "/etc/omnet/")
+
+debugFilename = os.path.join(logDir, "dissomniag_DEBUG")
+
+warningFilename = os.path.join(logDir, "dissomniag_WARNING")
+
+logToStdOut = True
+
+rpcServerPort = 8008
+
+SSL = True
+
+pidFile = "/var/run/dissomniag_live.pid"
+
+#Generate with: openssl req -new -x509 -days 365 -nodes -out cert.pem -keyout key.pem
+sslPrivateKey = "/etc/ssl/private/ssl-cert-snakeoil.key"
+sslCertFile = "/etc/ssl/certs/ssl-cert-snakeoil.pem"
+
