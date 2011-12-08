53021be5ea00a8211c6b471a1e6a03361ddb8862
Sebastian Wallat <sebastian.wallat@uni-due.de>
2011-12-08 09:46:36 +0100

    DeployToRunningImage.sh added

---
+++ DeployToRunningImage.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -x
+sshKeyFile=/home/sw/.ssh/id_rsa.pub
+ip=132.252.154.110
+tarBallName=dissomniagLive.tar.gz
+targetFolder=/usr/share/dissomniag-live/
+
+#Add ssh Key on remote
+ssh-copy-id -i $sshKeyFile root@$ip
+
+./createLiveDaemonTarBall.py
+
+scp $tarBallName root@$ip:
+
+ssh root@$ip tar xvfz $tarBallName -C /
