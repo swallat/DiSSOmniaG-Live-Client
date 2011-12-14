41f175c38fd0c6d392949c7589e2588f2a52673d
Sebastian Wallat <sebastian.wallat@uni-due.de>
2011-12-14 10:29:49 +0100

    License added

--- dissomniagLive/server.py
+++ dissomniagLive/server.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
-"""
-Created on 06.10.2011
-
-@author: Sebastian Wallat
-"""
-
+# DiSSOmniaG (Distributed Simulation Service wit OMNeT++ and Git)
+# Copyright (C) 2011, 2012 Sebastian Wallat, University Duisburg-Essen
+# 
+# Based on an idea of:
+# Sebastian Wallat <sebastian.wallat@uni-due.de, University Duisburg-Essen
+# Hakim Adhari <hakim.adhari@iem.uni-due.de>, University Duisburg-Essen
+# Martin Becke <martin.becke@iem.uni-due.de>, University Duisburg-Essen
+#
+# DiSSOmniaG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DiSSOmniaG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DiSSOmniaG. If not, see <http://www.gnu.org/licenses/>
 import socket
 import socketserver
 import ssl
